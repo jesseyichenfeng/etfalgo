d56920f8b14d4f5e62cf13f97d1dd9be1cfe6031
Yichen Feng <yichenfeng@Yichens-MacBook-Pro.local>
2022-12-02 23:53:22 -0500

    init

--- main.py
+++ main.py
@@ -3,12 +3,11 @@ import pandas as pd
 import pandas_ta as ta
 import os
 from experiment.function import pull_data
-start_date = '2013-01-01'
+
+start_date = "2013-01-01"
 
 if __name__ == "__main__":
 
-    tiingo_token = os.getenv('TIINGO')
-    data = pull_data(ticker='UPRO', start_date=start_date)
-    benchmark_data = pull_data(ticker='VOO', start_date=start_date)
-
-
+    tiingo_token = os.getenv("TIINGO")
+    data = pull_data(ticker="UPRO", start_date=start_date)
+    benchmark_data = pull_data(ticker="VOO", start_date=start_date)
